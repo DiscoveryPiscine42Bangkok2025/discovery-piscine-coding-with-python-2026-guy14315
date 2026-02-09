c1680be264afbb80ec038a2d9f17b24a717cb8bd
kobkarn <guygame97@gmail.com>
2026-02-09 22:57:55 +0700

    fix cell02/ex03

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -4,7 +4,7 @@ number1 = int(input("Enter first number:\n"))
 number2 = int(input("Enter second number:\n"))
 result = number1 * number2
 
-print(f'{number1} x {number2}\n')
+print(f'{number1} x {number2} = {result}\n')
 
 if result == 0:
     print("This result is positive and negative.")
